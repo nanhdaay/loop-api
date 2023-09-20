7c37ca9dbe5edb7b4013121437546533083def2f
DangNguyen <nguyendangit04@gmail.com>
2023-09-20 23:05:03 +0700

    add cors

--- index.js
+++ index.js
@@ -1,46 +1,64 @@
 import express from "express";
-const app = express ();
-import userRoutes from "./routes/users.js"
-import postRoutes from "./routes/posts.js"
-import commentRoutes from "./routes/comments.js"
-import likeRoutes from "./routes/likes.js"
-import relationshipRoutes from "./routes/relationship.js"
-import authRoutes from "./routes/auth.js"
+const app = express();
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import relationshipRoutes from "./routes/relationship.js";
+import authRoutes from "./routes/auth.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import multer from "multer";
 
-app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Credentials", true)
-    next()
-})
-app.use(express.json())
-app.use(cors())
+var whitelist = [
+  "localhost:3000",
+  "localhost",
+  "http://localhost",
+  "http://localhost:3000",
+  "https://loop-fe.onrender.com",
+  "loop-fe.onrender.com",
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(express.json());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, '../client/public/upload')
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + file.originalname);
-    }
-})
-
-const upload = multer({storage: storage});
-
-app.post("/api/upload", upload.single("file"), (req,res)=>{
-    const file = req.file;
-    res.status(200).json(file.filename);
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
 });
 
-app.use("/api/users", userRoutes)
-app.use("/api/posts", postRoutes)
-app.use("/api/comments", commentRoutes)
-app.use("/api/likes", likeRoutes)
-app.use("/api/relationships", relationshipRoutes)
-app.use("/api/auth", authRoutes)
+const upload = multer({ storage: storage });
 
-app.listen(6868, ()=>{
-    console.log("API working!")
-})
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/relationships", relationshipRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(6868, () => {
+  console.log("API working!");
+});
